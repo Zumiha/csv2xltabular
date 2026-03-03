2c75d68edfb2eb1da3519d9283e8f3baf94a3dcb
Zub Mik <zubetzmikhael@gmail.com>
2026-03-03 16:54:17 +0300

    reworked table render logic. now looks for divider with suitable remainder (assigns in class); divider - column number for bigger tables, remainder - column number for left over table.

--- csv2xltabular.cpp
+++ csv2xltabular.cpp
@@ -15,40 +15,91 @@ CSVtoXLTABularConverter::~CSVtoXLTABularConverter()
     delete ini_parser_;
 }
 
+TableConfig CSVtoXLTABularConverter::calculateTableConfig(int _header_size) {
+    TableConfig table_settings{};
+    auto effective_columns = _header_size - 1; std::cout << "\neffective_columns: " << effective_columns << "\n";
+    for (int i = max_columns_; i >= 1; --i) {
+        std::cout << "Counting loop: " << i << "\t";
+        int k = effective_columns / i; std::cout << "k: " << k << "\t";
+        int r = effective_columns % i; std::cout << "r: " << r << "\n";
+        
+        if ((effective_columns / i == 1 && r == 0) || effective_columns / i == 0) {
+            std::cout << "CSV columns fit in one table: " << effective_columns;
+            table_settings.tables_rows_config.push_back({false, 1, effective_columns});
+            table_settings.table_column_width = static_cast<float>(table_settings.columns_sum_width) / table_settings.tables_rows_config[0].col_end;
+            std::cout << " with column width: " << table_settings.table_column_width << " mm\n";
+            return table_settings;    
+        }
+
+        if (r == 0 || r >= this->remdnr_min_) {
+            std::cout << "\nNumber of tables with " << i << " columns: " << k << "\n";
+            // table_settings.table_count = i; table_settings.max_columns = k;
+            int col_start = 1, col_end = i;
+            for (int j = 0; j < k; ++j) {
+                table_settings.tables_rows_config.push_back({false, col_start, col_end});
+                std::cout << "Table " << j + 1 << ": col_start = " << col_start << ", col_end = " << col_end << "\n";
+                col_start += i;
+                col_end += i;
+            }
+
+
+            if (r > 0) {
+                table_settings.tables_rows_config.push_back({true, col_start, col_start + r - 1});
+                std::cout << "Plus table with " << r << " columns: ";
+                // table_settings.remain_columns = r;
+                std::cout << "col_start = " << col_start << ", col_end = " << (col_start + r - 1) << "\n";
+            }
+
+            table_settings.table_column_width = static_cast<float>(table_settings.columns_sum_width) / table_settings.tables_rows_config[0].col_end;
+            std::cout << " with column width: " << table_settings.table_column_width << " mm\n";
+
+            std::cout << std::endl;
+            return table_settings;  
+        }
+    }
+    return table_settings; // Default return, should not reach here
+}
+
 void CSVtoXLTABularConverter::convert()
 {
     std::setlocale(LC_ALL, "Russian"); // Set locale to the user's environment default
     auto table = csv_parser_->parse_all();
 
     auto header = table[1];
-    int num_columns = static_cast<int>(header.size()); 
-    std::cout << "num_columns: " << num_columns << "\n";
-    
-    int quantity, column_start, column_end;
+    int header_size = static_cast<int>(header.size()); 
+    std::cout << "\nheader_size: " << header_size << "\n";
+
+    int table_count, table_rows, column_start, column_end;
     column_start =  1;
-    if (num_columns > max_columns_) {
-        column_end = max_columns_;
-        quantity = (num_columns + max_columns_ - 2)/max_columns_;
-        std::cout << "quantity: " << quantity << "\n\n";
-    } else {
-        column_end = num_columns;
-        quantity = 1;
-    }
+
+    auto table_config_ = calculateTableConfig(header_size);
+
     std::string header_line;
 
-    for (int i = 1; i <= quantity; i++) {
+    for (int i = 0; i < table_config_.tables_rows_config.size(); i++) {
         // Render header line
-        std::cout << "column_start: " << column_start << " column_end: " << column_end << "\n";
-        header_line = headerLineRender(column_start, column_end, header);
+        int cell_start = table_config_.tables_rows_config[i].col_start;
+        int cell_end = table_config_.tables_rows_config[i].col_end;
+        int table_size = cell_end - cell_start + 1;
+
+        std::cout << "\ncolumn_start: " << cell_start << " column_end: " << cell_end << "\n";
+        header_line = headerLineRender(
+            cell_start, 
+            cell_end, 
+            header
+        );
         std::cout << header_line << std::endl;
         // LaTeX tabular format
-        tableRender(column_start, column_end, table, header_line);
 
-        column_start += max_columns_;
-        column_end += max_columns_;
-        if (column_end > num_columns) {
-            column_end = (num_columns - 1);
-        }
+        tableRender(
+            table_config_.table_width, 
+            table_config_.table_column_width,  
+            table_size, 
+            cell_start, 
+            cell_end, 
+            table, 
+            header_line
+        );
     } 
 }
 
@@ -74,10 +125,18 @@ std::string CSVtoXLTABularConverter::headerLineRender(int start_cell, int end_ce
     return line;
 }
 
-void CSVtoXLTABularConverter::tableRender(int start_cell, int end_cell, const std::map<int, std::vector<std::string>>& table_, const std::string& header_line_) {
+void CSVtoXLTABularConverter::tableRender(
+    int _table_width, 
+    float _column_width,  
+    int table_size, 
+    int start_cell, 
+    int end_cell, 
+    const std::map<int, std::vector<std::string>>& table_, 
+    const std::string& header_line_
+) {
     auto table_width = end_cell - start_cell + 1;
     latex_string_ += "\\setlength\\LTleft{0cm} % Adjust the value as needed\n"
-                    "\\begin{xltabular}{180mm}{|c|*{" + std::to_string(table_width) + "}{m{8.5mm}|}}\n"
+                    "\\begin{xltabular}{" + std::to_string(_table_width) + "mm}{|c|*{" + std::to_string(table_width) + "}{m{" + std::to_string(_column_width) + "mm}|}}\n"
                     "\\hline\n"
                     "\\diagbox{час}{L,мм} & ";
     latex_string_ += header_line_;
--- csv2xltabular.h
+++ csv2xltabular.h
@@ -8,6 +8,22 @@
 #include <fstream>
 #include <string>
 
+struct TablesRows {
+    bool remain_table = false;
+    int col_start;
+    int col_end;
+};
+
+class TableConfig {
+public:
+    std::vector<TablesRows> tables_rows_config;
+    
+    int table_width = 180; // mm, total width of the table in LaTeX
+    int columns_sum_width = 102; // mm, sum of column widths (excluding the first column)
+    float table_column_width; // mm, width of each column in the main tables
+
+};
+
 class CSVtoXLTABularConverter {
 public:
     CSVtoXLTABularConverter() = delete;
@@ -19,18 +35,36 @@ public:
     // Convert CSV to LaTeX tabular format based on INI configuration
     void convert();  
     void exportToFile(const std::string& output_filename = "csv_table.tex");
-
+    
     
     private:
     CSVParser* csv_parser_;
     IniParser* ini_parser_;
     
-    std::string lang_ = "rus"; // Default language
+    
     int max_columns_ = 12; // Render default max columns per page
+    int remdnr_min_ = 8; // Minimum remainder
+    
+    TableConfig table_config_;
+    
+    std::string lang_ = "rus"; // Default language
     std::string latex_string_ = ""; // LaTeX tabular format string
     
-    std::string headerLineRender(int start_cell, int end_cell, const std::vector<std::string>& header_);
-    void tableRender(int start_cell, int end_cell, const std::map<int, std::vector<std::string>>& table_, const std::string& header_line_);
+    std::string headerLineRender(
+        int start_cell, 
+        int end_cell, 
+        const std::vector<std::string>& header_
+    );
+    void tableRender(
+        int _table_width, 
+        float _column_width, 
+        int table_size, 
+        int start_cell, 
+        int end_cell, 
+        const std::map<int, std::vector<std::string>>& table_, 
+        const std::string& header_line_
+    );
+    TableConfig calculateTableConfig(int _header_size);
 };
 
 #endif // CSV2XTABULAR_H
